cec9de990010710948d3eea750f479b4781472b2
St-Creamer <47365313+St-Creamer@users.noreply.github.com>
2021-02-02 03:34:34 +0100

    added full crud

--- routes/ItemRoute.js
+++ routes/ItemRoute.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const Item = require("../models/Item");
 
 //Item Routes
+//get all
 router.get("/items", (req, res) => {
   const result = Item.find();
   if (!result) res.status(404).send("No data found");
   res.status(200).json(result);
 });
 
+//create
 router.post("/", async (req, res) => {
   const itemname = req.body.name;
   console.log(`recieved input: ${itemname}`);
@@ -22,5 +24,36 @@ router.post("/", async (req, res) => {
     res.send("item saved");
   });
 });
+
+//get one
+router.get("/:id", async (req, response) => {
+  await Item.findById(req.params.id,(err,res)=>{
+    if(err || !res){
+      return response.send("no data found"+err).status(404)
+    }
+    return response.send(res).status(200)
+  })
+});
+
+//delete one
+router.get("/delete/:id", async (req, response) => {
+  await Item.findByIdAndDelete(req.params.id,(err,res)=>{
+    if(err){
+      return response.send(err).status(404)
+    }
+    return response.send("document deleted").status(200)
+  })
+});
+
+//update
+router.put("/put/:id", async (req, response) => {
+  await Item.findByIdAndUpdate(req.params.id,{name : req.body.name},(err,res)=>{
+    if(err){
+      return response.send(err).status(404)
+    }
+    return response.send("document updated").status(200)
+  })
+});
+
 
 module.exports = router;
